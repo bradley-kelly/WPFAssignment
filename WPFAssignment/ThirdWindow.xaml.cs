397c8ff77f3a251605bdaeb8dd3da6b739afe637
BRADLEY KELLY <bradley.kelly@student.centralia.edu>
2021-03-16 03:18:50 -0700

    Add project files.

---
+++ WPFAssignment/ThirdWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFAssignment
+{
+    /// <summary>
+    /// Interaction logic for ThirdWindow.xaml
+    /// </summary>
+    public partial class ThirdWindow : Window
+    {
+        public ThirdWindow()
+        {
+            InitializeComponent();
+        }
+        private void mainWindow_Click(object sender, RoutedEventArgs e)
+        {
+            var mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close();
+        }
+    }
+}
